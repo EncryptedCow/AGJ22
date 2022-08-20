9068287e7bcb95f78ba0698104c07cba2e759421
EncryptedCow <encryptedcow@gmail.com>
2022-08-20 14:49:41 -0600

    Temp mute of music player

--- scripts/music.gd
+++ scripts/music.gd
@@ -1,9 +1,10 @@
 extends Node
 
-var audio_stream: AudioStreamPlayer = AudioStreamPlayer.new()
+var music_player_scene = preload("res://game_objects/music_player.tscn")
 var main_loop = preload("res://audio/agjMainDRAFT05.mp3")
 
 func _ready() -> void:
+	var audio_stream = music_player_scene.instance()
 	add_child(audio_stream)
 	audio_stream.stream = main_loop
 	audio_stream.play()
