c07243e884719c7742ef18a0b8f37453375a4722
EncryptedCow <encryptedcow@gmail.com>
2022-08-20 14:48:51 -0600

    Added player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,8 @@ export (CAMERA_MODE) var camera_mode = CAMERA_MODE.Follow
 
 var velocity := Vector2.ZERO
 
+export var anim_override: bool = false
+
 func _physics_process(delta: float) -> void:
 	var movement = int(Input.is_action_pressed("move_right")) - int(Input.is_action_pressed("move_left"))
 	velocity.x = movement * speed
@@ -18,3 +20,23 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
 			velocity.y = -jump_speed
+
+func _process(delta: float) -> void:
+	var sprite: Sprite = get_node("Sprite")
+	var animator: AnimationPlayer = get_node("AnimationPlayer")
+	
+	if velocity.x > 0:
+		sprite.flip_h = false
+	elif velocity.x < 0:
+		sprite.flip_h = true
+	
+	if not anim_override:
+		if not is_on_floor():
+			animator.play("jump")
+		elif abs(velocity.x) > 0:
+			if abs(velocity.x) < 100:
+				animator.play("walk")
+			else:
+				animator.play("run")
+		else:
+			animator.play("idle")
