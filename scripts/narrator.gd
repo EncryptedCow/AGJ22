a29064fff5dddb1ff6fe039c1cea6fb99bd3ef15
EncryptedCow <encryptedcow@gmail.com>
2022-08-20 21:04:58 -0600

    Update narrator.gd

--- scripts/narrator.gd
+++ scripts/narrator.gd
@@ -2,13 +2,18 @@ class_name Narrator
 extends CanvasLayer
 
 signal line_complete()
+signal request_next_line()
 
-onready var caption_label: Label = get_node("CenterContainer/Label")
-onready var caption_tween: Tween = get_node("CenterContainer/Label/Tween")
+onready var caption_label: Label = get_node("CenterContainer/MarginContainer/VBoxContainer/Label")
+onready var caption_tween: Tween = get_node("CenterContainer/MarginContainer/VBoxContainer/Label/Tween")
 
 export var line_time: float = 2.0
 
+var line_complete: bool = false
+
 func say_line(text: String):
+	line_complete = false
+	
 	caption_label.percent_visible = 0
 	caption_label.text = text
 	
@@ -16,4 +21,16 @@ func say_line(text: String):
 	caption_tween.start()
 
 func _on_tween_completed(object: Object, key: NodePath) -> void:
+	line_complete = true
 	emit_signal("line_complete")
+
+func finish_line():
+	if caption_tween.is_active():
+		caption_tween.stop_all()
+		caption_label.percent_visible = 1
+	else:
+		emit_signal("request_next_line")
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("skip_dialogue"):
+		finish_line()
